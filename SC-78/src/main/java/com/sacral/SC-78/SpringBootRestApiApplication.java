8953d121651496e8d3a6fac7d07fe59145da7e6b
anandvechalapu <anand.vechalapu@gmail.com>
2023-04-20 16:03:27 +0530

    initial commit

---
+++ SC-78/src/main/java/com/sacral/SC-78/SpringBootRestApiApplication.java
@@ -0,0 +1,12 @@
+package com.sacral.SC-78;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRestApiApplication.class, args);
+    }
+}
